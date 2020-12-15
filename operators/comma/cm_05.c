e170a9068ba36e74b7688e048e2ae8d768d1f16e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-15 11:48:13 +0300

    Create cm_05.c

---
+++ operators/comma/cm_05.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//bu koddaki hiçbirvirgül operatör değil
+
+struct Data {
+	int m1, m2, m3;
+};
+
+void func(int x, int y)
+{
+	//...
+}
+
+int main()
+{
+	int x = 10, y = 20, z = 30;
+	int a[] = { 1, 2, 3, 4, 5 };
+
+	func(x, y);
+}
