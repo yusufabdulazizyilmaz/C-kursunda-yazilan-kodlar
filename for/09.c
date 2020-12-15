c903e9948f05fd8baed8ef1d2bc811c2e637c98d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-15 18:37:44 +0300

    Create 09.c

---
+++ for/09.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	return n < 2 ? 1 : n * factorial(n - 1);
+}
+
+int main()
+{
+	double sum = 0.;
+
+	for (int i = 0; i < 10; ++i) {
+		sum += 1. / factorial(i);
+	}
+
+	printf("sum = %f\n", sum);
+}
