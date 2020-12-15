22fb3496f1a0f286e0d8d27f87f0fb0b0479db98
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-15 18:34:00 +0300

    Create 13.c

---
+++ for/13.c
@@ -0,0 +1,11 @@
+// tek bir karakter ekle ya da tek bir karakter değiştir, çalışacak program ekrana 5 kez necati yazsın
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 5;
+
+	for (int i = 0; i < n; i--)
+		printf("necati\n");
+}
