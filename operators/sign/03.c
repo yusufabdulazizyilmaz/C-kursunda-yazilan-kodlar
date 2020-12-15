cd451379e3673c4746667bab58b89d6e65778b88
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-15 10:33:26 +0300

    Create 03.c

---
+++ operators/sign/03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//sign operator -
+//-x işlemi 0 - x anlamındadır.
+//işaret operatörü -'nin ysn etkisi yoktur
+
+int main()
+{
+	int x = 8;
+
+	printf("x  = %d\n", x);
+	printf("-x = %d\n", -x);
+
+	printf("x = %d\n", x);
+}
