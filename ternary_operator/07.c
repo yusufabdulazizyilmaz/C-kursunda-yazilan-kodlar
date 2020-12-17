9563353e234990f4844e1af38e60154ee5cea4a4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-17 14:58:15 +0300

    Create 07.c

---
+++ ternary_operator/07.c
@@ -0,0 +1,24 @@
+int func1(int val)
+{
+	//
+	return val == 10 ? 1 :
+		val == 20 ? 3 :
+		val == 40 ? 7 :
+		0;
+}
+
+
+int func(int val)
+{
+	//
+	if (val == 10)
+		return 1;
+
+	if (val == 20)
+		return 3;
+
+	if (val == 40)
+		return 7;
+
+	return 0;
+}
