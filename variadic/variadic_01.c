666733daf093ddf82136b298081010afb6c1256b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-26 07:52:36 +0300

    Create variadic_01.c

---
+++ variadic/variadic_01.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum(int count, ...)
+{
+	int sum = 0;
+	va_list args;
+
+	va_start(args, count);
+
+	for (int i = 0; i < count; i++) {
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return sum;
+}
+
+int main()
+{
+	int a = sum(2, 15, 20);
+	int b = sum(3, 10, 15, 20);
+	int c = sum(4, a, b, 10, 10);
+
+	printf("c = %d\n", c);
+}
