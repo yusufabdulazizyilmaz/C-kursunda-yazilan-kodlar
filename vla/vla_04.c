85dc054acaa3f12db78b479d7d5c5938985f9a3b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-12-26 08:06:57 +0300

    Create vla_04.c

---
+++ vla/vla_04.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	size_t row, col;
+        srand((unsigned)time(NULL));
+
+	printf("satir ve sutun sayisi: ");
+	scanf("%zu%zu", &row, &col);
+	int(*p)[row][col] = malloc(sizeof(*p));
+        //int *pd = malloc(row * col * sizeof(int));
+	if (!p) {
+		fprintf(stderr, "bellek yetersiz\n");
+		return 1;
+	}
+
+	for (size_t i = 0; i < row; ++i) {
+		for (size_t k = 0; k < col; ++k) {
+                        //pd[i * col + k] = rand() % 10;
+			(*p)[i][k] = rand() % 10;
+		}
+	}
+
+    //...
+    
+    for (size_t i = 0; i < row; ++i) {
+		for (size_t k = 0; k < col; ++k) {
+			printf("%d", (*p)[i][k]);
+		}
+        printf("\n");
+	}
+	
+    free(p);
+}
