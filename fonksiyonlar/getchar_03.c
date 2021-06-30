844fe46cceab2fcd35c04fc07e687da35ac604e0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-30 15:58:27 +0300

    Create getchar_03.c

---
+++ fonksiyonlar/getchar_03.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	int ch;
+
+	printf("evet mi (e) hayir mi (h) :  ");
+
+	ch = getchar();
+	if (ch == 'e')
+		printf("evet dediniz\n");
+	else if (ch == 'h')
+		printf("hayir dediniz\n");
+	else
+		printf("gecersiz cevap\n");
+}
