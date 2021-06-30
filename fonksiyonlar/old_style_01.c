d1f53c483865204f4e5463a9c254b83518769f06
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-06-30 16:22:58 +0300

    Create old_style_01.c

---
+++ fonksiyonlar/old_style_01.c
@@ -0,0 +1,25 @@
+//old style function definition
+sum (a, b, c)
+{
+	return a + b + c;
+}
+
+//old style function definition
+double mean(x, y)
+double x, y;
+{
+	return (x + y) / 2.;
+}
+
+
+//günümüzdeki biçim
+int sum(int a, int b, int c)
+{
+	return a + b + c;
+}
+
+
+double mean(double x, double y)
+{
+	return (x + y) / 2.;
+}
